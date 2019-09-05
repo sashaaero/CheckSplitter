a207c5e1b9b2cb063d16dff082b37ee5dd3553f4
Dominov <dominovtut@gmail.com>
2019-09-05 16:03:05 +0300

    Added login form and registration form

--- form_request.py
+++ form_request.py
@@ -3,6 +3,53 @@ from wtforms.validators import *
 from models import *
 
 
+def nickname_free(form, field):
+	nickname = field.data
+	chek = User.get(nickname=nickname)
+	if chek is not None:
+		raise ValidationError('Nickname %r is already taken' % nickname)
+	
+
+def len_check(form, field):
+	pwd = field.data
+	if len(pwd) < 5:
+		raise ValidationError('Your password is too short, use 5 or more characters')
+
+def pwd_check(form, field):
+	pwd2 = field.data
+	pwd1 = form.pwd1.data
+	if pwd1 != pwd2:
+		raise ValidationError('Passwords should match')
+
+def nickname_check(form, field):
+	nickname = field.data
+	check = User.get(nickname=nickname)
+	if check is None:
+		raise ValidationError('User with nickname %r not found' % nickname)
+
+def pwd_match_check(form, field):
+	nickname = form.nickname.data
+	pwd = field.data
+	check = User.get(nickname=nickname, pwd=pwd)
+	if check is None:
+		raise ValidationError('Wrong password. Try again')
+
+
+
+
+		
+
+class RegForm(Form):
+	nickname = StringField('Nickname', [InputRequired(), nickname_free])
+	fullname = StringField('Fullname', [InputRequired()])
+	pwd1 = PasswordField('Password', [InputRequired(), len_check])
+	pwd2 = PasswordField('Password again', [InputRequired(), pwd_check])
+
+
+class LoginForm(Form):
+	nickname = StringField('Nickname', [InputRequired(), nickname_check])
+	pwd = PasswordField('Password', [InputRequired(), pwd_match_check])
+
 class OrderItem(Form):
     title = StringField('Название блюда', [InputRequired()])
     price = StringField('Цена', [InputRequired()])
